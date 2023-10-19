d602e0f83ad7df8fb0c298af8d5adca71ff25c4e
meozalat2022 <meozalat2002@hotmail.com>
2023-10-19 15:36:52 +0300

    adding latest meals card

--- app/components/LatestTenMeals/LatestTenMeals.jsx
+++ app/components/LatestTenMeals/LatestTenMeals.jsx
@@ -4,30 +4,32 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchLatestMeals } from "../../redux/reducers/mealSlice";
 import styles from "./LatestTenMeals.module.css";
 import Card from "../UI/Card/Card";
+import { CATEGORIES } from "../../data/categories";
+import { MEALS } from "../../data/meals";
 const LatestTenMeals = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     // dispatch(fetchLatestMeals());
   }, []);
-  const latestTenMeals = useSelector((state) => state.meals.latestMeals);
+  // const latestTenMeals = useSelector((state) => state.meals.latestMeals);
 
-  if (!latestTenMeals || latestTenMeals.length < 1) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <h3>Loading...</h3>
-      </div>
-    );
-  }
+  // if (!latestTenMeals || latestTenMeals.length < 1) {
+  //   return (
+  //     <div
+  //       style={{
+  //         display: "flex",
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //       }}
+  //     >
+  //       <h3>Loading...</h3>
+  //     </div>
+  //   );
+  // }
   return (
-    <div className={styles.container}>
-      {latestTenMeals.map((item, index) => {
+    <div className="flex flex-wrap justify-center">
+      {MEALS.map((item, index) => {
         return <Card meals={item} key={item.id} />;
       })}
     </div>
--- app/components/UI/Card/Card.jsx
+++ app/components/UI/Card/Card.jsx
@@ -2,32 +2,38 @@ import Image from "next/image";
 import styles from "./Card.module.css";
 import { useSelector } from "react-redux";
 import Link from "next/link";
+import { CATEGORIES } from "../../../data/categories";
 const Card = ({ meals, key, catId }) => {
   const cat = useSelector((state) => state.category.data);
-  const catTitle = cat.find((item) => item.id === meals.categoryId);
+  const catTitle = CATEGORIES.find((item) => item.id === meals.categoryId);
   const ingredientsArr = meals.ingredients.slice(0, 2);
   return (
-    <div className={styles.container}>
-      <Link href={`/showRecipe/${meals.id}`}>
-        <h3 className={styles.title}>{meals.title}</h3>
+    <div className=" w-full shadow-lg rounded-md mt-1 md:w-1/2 lg:w-1/4 xl:w-1/6  px-2 flex flex-col justify-between items-center p-1 mx-1">
+      <Link className="w-full h-full " href={`/showRecipe/${meals.id}`}>
+        <h3 className="text-center overflow-hidden text-primary text-sm">
+          {meals.title}
+        </h3>
       </Link>
       <Link href={`/showRecipe/${meals.id}`}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            fill={true}
-            src={meals.imageUrl[0]}
+        <div className="-z-10 relative ">
+          <img
+            className="w-36 h-36 p-1 rounded-md border border-double border-b-4 border-accent "
+            src={meals.imagUrl}
+            // src={meals.imageUrl[0]}
             alt={meals.title}
           />
         </div>
       </Link>
-      <ul>
-        <li className={styles.list}>
-          <h5 className={styles.ingredients}>{ingredientsArr}...شاهد المزيد</h5>
-        </li>
-      </ul>
-      <Link href={`/mealsList/${catTitle && catTitle.id}`}>
-        <p className={styles.categoryTitle}>{catTitle && catTitle.title}</p>
+      <div className="w-full h-full flex justify-center items-center">
+        <h5 className={`${styles.ingredients} text-center mb-2`}>
+          {ingredientsArr}...شاهد المزيد
+        </h5>
+      </div>
+      <Link
+        className=" w-full h-full"
+        href={`/mealsList/${catTitle && catTitle.id}`}
+      >
+        <p className="text-accent font-bold">{catTitle && catTitle.title}</p>
       </Link>
     </div>
   );
